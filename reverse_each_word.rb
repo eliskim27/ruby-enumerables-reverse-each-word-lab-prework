110d83ed6b06bb7d17c5898f2f76f690fabd8003
Eli Kim <eliskim27@gmail.com>
2019-10-16 01:43:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,15 @@
+#def reverse_each_word (string)
+#  s_array = string.split
+#  s_array.each do |num| 
+#    num.reverse!
+#  end
+#  s_array.join(' ')
+#end
+
 def reverse_each_word (string)
   s_array = string.split
- 
-  s_array.each do |num| 
-    num.reverse!
-  end
- 
+  
+  s_array.collect
+  
   s_array.join(' ')
 end
